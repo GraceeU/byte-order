2b7049bccc909cfa48241e9a2c7a343844b00222
Gracee_U <36237958+GraceeU@users.noreply.github.com>
2020-07-14 11:20:42 +0900

    Update add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -70,4 +70,4 @@ int main(void){
 uint32_t htonl(uint32_t n){
         return (n & 0xFF000000) >>24 | (n & 0x00FF0000) >>8 |(n & 0x0000FF00) << 8 | (n & 0x000000FF) >> 24 ;
 }
-uint32_t htonl(uint32_t n);
+
